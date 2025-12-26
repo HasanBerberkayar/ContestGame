387ab64a33dc1edc4a58dc0695212d5eda47e68d
HasanBerberkayar <119157476+HasanBerberkayar@users.noreply.github.com>
2025-12-26 14:55:54 +0300

    GameProto

--- CourseContestGame/Assets/Scripts/EnemyBehavior.cs
+++ CourseContestGame/Assets/Scripts/EnemyBehavior.cs
@@ -1,27 +1,95 @@
-using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    //1
-    private void OnTriggerEnter(Collider other)
+    public Transform PatrolRoute;
+    public List<Transform> Locations;
+    public int _locationIndex = 0;
+    public NavMeshAgent _agent;
+    public Transform Player;
+
+    public int _lives = 3;
+
+    public int EnemyLives
     {
-        Debug.Log("AUUUUU");
-        //2
-        if (other.name == "Player")
+        get { return _lives; }
+        set
         {
-            Debug.Log("Player detected – attack!");
+            _lives = value;
+
+            if (_lives <= 0)
+            {
+                Destroy(this.gameObject);
+                Debug.Log("Enemy down...");
+            }
         }
     }
 
-    //3
+    /*
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        Player = GameObject.Find("Player").transform;
+
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
+    }
+
+    private void Update()
+    {
+        if (_agent.remainingDistance < 0.2f && !_agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+    */
+
+    public void InitializePatrolRoute()
+    {
+        foreach (Transform child in PatrolRoute)
+        {
+            Locations.Add(child);
+        }
+    }
+
+    public void MoveToNextPatrolLocation()
+    {
+        if (Locations.Count == 0)
+        {
+            return;
+        }
+
+        _agent.destination = Locations[_locationIndex].position;
+        _locationIndex = (_locationIndex + 1) % Locations.Count;
+    }
+
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            _agent.destination = Player.position;
+            Debug.Log("Player detected - attack!");
+        }
+    }
+
     private void OnTriggerExit(Collider other)
     {
-        //4
         if (other.name == "Player")
         {
             Debug.Log("Player out of range, resume patrol");
+        }
+    }
+    */
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Bullet(Clone)")
+        {
+            EnemyLives -= 1;
+            Debug.Log("Critical hit!!!");
         }
     }
 }
